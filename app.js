531d0ebfa1a8fe595656d74adff45484c3c4427b
Alejo De La Quintana <quintanazzale@gmail.com>
2022-04-03 20:59:32 -0300

    Subida de estructura de proyecto

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+const PORT = process.env.PORT || 3000;
+
+//app.use(express.static(path.resolve(__dirname, "./public")))
+app.use(express.static("public"));
+
+app.get('/', function (req, res) {
+    let htmlPath = path.resolve(__dirname, './src/views/home.html');
+    res.sendFile(htmlPath);
+})
+
+app.listen(PORT, () => console.log('Servidor corriendo en el puerto ' + PORT))
