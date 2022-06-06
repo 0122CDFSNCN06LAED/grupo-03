f6c6637564b50ab9b2bc799d818c0178edea18c4
Mario-Pelavski <mpelavski48@gmail.com>
2022-06-06 15:05:22 -0300

    se comenzo a implementar las funcionalidades de base de datos

---
+++ src/database/models/users.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, dataTypes) => {
+    const alias = "User";
+    const columns = {
+        id: {
+            type: dataTypes.BIGINT(10).UNSIGNED,
+                primaryKey: true,
+                allowNull: false,
+                autoIncrement: true
+        },
+        nombre: {
+            type: dataTypes.STRING(100),
+            allowNull: false
+        },
+        email: {
+            type: dataTypes.STRING(100),
+            allowNull: false
+        },
+        telefono: {
+            type: dataTypes.STRING(100),
+            allowNull: false
+        },
+        password: {
+            type: dataTypes.STRING(100),
+            allowNull: false
+        },
+        imagen: {
+            type: dataTypes.STRING(100),
+            allowNull: false
+        },
+    };
+  
+    const config = {
+      timestamps: false,
+      deletedAt: false
+    };
+  
+    const User = sequelize.define(alias, columns, config);
+  
